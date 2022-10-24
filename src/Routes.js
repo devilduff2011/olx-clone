7069934c2c3cc30bfd4f3af6139c33311d3f3385
Matheus Ribeiro Henriques <matheusribeiro12341@hotmail.com>
2022-10-24 16:23:59 -0300

    Adicionando a página 404

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home';
 import About from './pages/About';
+import NotFound from './pages/NotFound/NotFound';
 
 export default () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/sobre" element={<About />}/>
+            <Route path='*' element={<NotFound/>} />
         </Routes>
 
     );
